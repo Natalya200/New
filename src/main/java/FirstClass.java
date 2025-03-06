8738f335482ab18bbb7347dc3c84eaf900119b93
Natali <nskachkova5663@gmail.com>
2025-03-06 14:09:05 +0400

    new class create

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,2 +1,26 @@
 public class FirstClass {
-}
+        public static void main(String[] args) {
+            printThreeWords();
+        }
+        public static void printThreeWords() {
+            System.out.println("Orange");
+            System.out.println("Banana");
+            System.out.println("Apple");
+        }
+
+        {
+
+            CheckSumSign();
+        }
+        public static void CheckSumSign() {
+            int a = 5;
+            int b = 6;
+            int sum = a + b;
+            if (sum >= 0) {
+                System.out.println("Сумма положительная");
+            } else {
+                System.out.println("Сумма отрицательная");
+            }
+        }
+
+    }
