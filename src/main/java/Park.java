b9b81364c579d0d607a4c54086e63eb1b3fddcef
Natali <nskachkova5663@gmail.com>
2025-03-06 15:49:30 +0400

    error done

--- src/main/java/Park.java
+++ src/main/java/Park.java
@@ -13,4 +13,3 @@ public class Park {
                 + time + " стоимость " + price );
     }
 }
-bfcb c
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,15 +4,15 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Product product = new Product("Телевизор", "10.09.2024", "Самсунг", "Китай", 2000.66, true);
-        Park park = new Park ("Орбита", "10:00-22:00", 500.4);
+        org.example.Product product = new org.example.Product("Телевизор", "10.09.2024", "Самсунг", "Китай", 2000.66, true);
+        org.example.Park park = new org.example.Park("Орбита", "10:00-22:00", 500.4);
 
-        Product[] productsArray = new Product[5];
-        productsArray[0] = new Product("Телевизор", "10.09.2024", "Самсунг", "Китай", 2000.66, true);
-        productsArray[1] = new Product("Телевизор", "10.09.2024", "Самсунг", "Китай", 2000.66, true);
-        productsArray[2] = new Product("Телевизор", "10.09.2024", "Самсунг", "Китай", 2000.66, true);
-        productsArray[3] = new Product("Телевизор", "10.09.2024", "Самсунг", "Китай", 2000.66, true);
-        productsArray[4] = new Product("Телевизор", "10.09.2024", "Самсунг", "Китай", 2000.66, true);
+        org.example.Product[] productsArray = new org.example.Product[5];
+        productsArray[0] = new org.example.Product("Телевизор", "10.09.2024", "Самсунг", "Китай", 2000.66, true);
+        productsArray[1] = new org.example.Product("Телевизор", "10.09.2024", "Самсунг", "Китай", 2000.66, true);
+        productsArray[2] = new org.example.Product("Телевизор", "10.09.2024", "Самсунг", "Китай", 2000.66, true);
+        productsArray[3] = new org.example.Product("Телевизор", "10.09.2024", "Самсунг", "Китай", 2000.66, true);
+        productsArray[4] = new org.example.Product("Телевизор", "10.09.2024", "Самсунг", "Китай", 2000.66, true);
 
         System.out.println(Arrays.toString(productsArray));
     }
