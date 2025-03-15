a7187da2ee28498917ce9e995e746813fe25d53e
Natali <nskachkova5663@gmail.com>
2025-03-15 14:53:46 +0400

    new lesson

---
+++ src/main/java/Student.java
@@ -0,0 +1,55 @@
+
+import java.util.*;
+public class Student {
+    private String name;
+    private String group;
+    private int course;
+    private List<Integer> grades;
+
+    public Student(String name, String group, int course, List<Integer> grades) {
+        this.name = name;
+        this.group = group;
+        this.course = course;
+        this.grades = grades;
+    }
+
+    public double getAverageGrade() {
+        if (grades.isEmpty()) return 0.0;
+        int sum = 0;
+        for (int grade : grades) {
+            sum += grade;
+        }
+        return (double) sum / grades.size();
+    }
+    // Метод для перевода на следующий курс
+    public void promote() {
+        course++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    @Override
+    public String toString() {
+        return "Студент: " + name + ", Группа: " + group + ", Курс: " + course + ", Оценки: " + grades;
+    }
+    // Метод для удаления студентов с низким средним баллом
+    public static void removeLow(Set<Student> students) {
+        students.removeIf(student -> student.getAverageGrade() < 3.0);
+    }
+
+    // Метод для печати студентов на заданном курсе
+    public static void printStudents(Set<Student> students, int course) {
+        System.out.println("Студенты на курсе " + course + ":");
+        for (Student student : students) {
+            if (student.getCourse() == course) {
+                System.out.println(student.getName());
+            }
+        }
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,36 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.example.Student.printStudents;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        PhoneBook phoneBook = new PhoneBook();
+phoneBook.add("Сидоров", "89273653423");
+phoneBook.add("Иванов", "89279371745");
+phoneBook.add("Васильев", "89175637382");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+System.out.println("Телефоны Сидорова: " + phoneBook.get("Сидоров"));
+System.out.println("Телефоны Иванова: " + phoneBook.get("Иванов"));
+System.out.println("Телефоны Васильева: " + phoneBook.get("Васильев"));
+
+        }
+    {
+        // Создание коллекции студентов
+        ArrayList<Object> students = new ArrayList<>();
+
+        // Добавление студентов в коллекцию
+        students.add(new Student("Иванов", "Группа A", 1, List.of(4, 5, 3)));
+        students.add(new Student("Петров", "Группа B", 1, List.of(2, 3, 4)));
+        students.add(new Student("Сидоров", "Группа A", 2, List.of(5, 5, 4)));
+        students.add(new Student("Кузнецов", "Группа B", 2, List.of(1, 2, 3)));
+
         }
     }
-}
+
+
+
