d1909e6a3ac7a70faf1055c264872f5e9d967605
Natali <nskachkova5663@gmail.com>
2025-04-01 21:07:02 +0400

    new lesson

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,8 @@ import static org.example.Student.printStudents;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+@SuppressWarnings("ALL")
 public class Main {
     public static void main(String[] args) {
         PhoneBook phoneBook = new PhoneBook();
